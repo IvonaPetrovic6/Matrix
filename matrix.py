1ad5c5e72138feccb7133319563e07d8d04bb9f2
Ivona Petrović <87564776+IvonaPetrovic6@users.noreply.github.com>
2021-07-17 12:24:23 +0200

    Add files via upload

--- matrix.py
+++ matrix.py
@@ -1,10 +1,14 @@
 matrix = [""]
-k = 1
-for i in range(10):
-    for j in range(10):
-        matrix.append(k)
-        print(matrix[k], end = " ")
-        k += 1
+x = int(input("Number of rows: "))
+y = int(input("Number of columns: "))
+
+for i in range(x):
+    for j in range(y):
+        matrix.append(input("> "))
+
+for i in range(x):
+    for j in range(y):
+        print(matrix[i][j], end = " ")
     print("")
 
 
